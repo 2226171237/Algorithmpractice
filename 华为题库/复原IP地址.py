c2db060e99f9a689549cf7fdbabfa04cd489f298
2226171237 <2226171237@qq.com>
2020-04-17 23:06:26 +0800

    huawei

--- 华为题库/复原IP地址.py
+++ 华为题库/复原IP地址.py
@@ -12,6 +12,37 @@
 class Solution:
     def restoreIpAddresses(self, s: str):
         '''
+        回溯
         :param str s:
         :return: list[str]
         '''
+        result=[]
+
+        def recoverIp(s:str,i:int,subIP:list,dot_num:int):
+            if i>len(s) or dot_num>3:
+                return
+            if i==len(s) and dot_num==3:
+                result.append(''.join(subIP))
+            oneNum=0
+            for j in range(i,min(i+3,len(s))):
+                oneNum=oneNum*10+ord(s[j])-ord('0')
+                if oneNum>255:
+                    break
+                if len(subIP)==0:
+                    subIP.append(str(oneNum))
+                    recoverIp(s, j + 1, subIP, dot_num)
+                    subIP.pop()
+                else:
+                    subIP.append('.')
+                    subIP.append(str(oneNum))
+                    recoverIp(s,j+1,subIP,dot_num+1)
+                    subIP.pop()
+                    subIP.pop()
+                if oneNum==0:
+                    break
+        recoverIp(s,0,[],0)
+        return result
+
+if __name__ == '__main__':
+    S=Solution()
+    print(S.restoreIpAddresses('0100210'))
